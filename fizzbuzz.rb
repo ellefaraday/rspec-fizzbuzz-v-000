57b9810dc82ebab5eddbdc8a350d34267e7dac1d
Elle Faraday <faraday.elle@gmail.com>
2017-10-15 01:26:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 = 0 && number % 5 = 0
+  if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
   end
-  elsif number % 3 = 0
+elsif number % 3 == 0
     puts "Fizz"
   end
-  elsif number % 5 = 0
+elsif number % 5 == 0
     puts "Buzz"
   end
  else
